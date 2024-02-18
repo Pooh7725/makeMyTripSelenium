a3bccef5d3c623c3a458f6a260bd27f2561d930f
Pooja Saini <sainipoojait@gmail.com>
2024-02-18 14:31:07 +0530

    adding base files

---
+++ src/main/java/PageClass/navigationBar.java
@@ -0,0 +1,61 @@
+package PageClass;
+
+public class navigationBar {
+
+    public void loginOrCreateAccount()
+    {
+
+    }
+    public void languageAndCurrency()
+    {
+
+    }
+    public void flights()
+    {
+
+    }
+    public void hotels()
+    {
+
+    }
+    public void homestaysAndVilla()
+    {
+
+    }
+    public void holidaypackages()
+    {
+
+    }
+    public void trains()
+    {
+
+    }
+    public void buses()
+    {
+
+    }
+    public void cabs()
+    {
+
+    }
+    public void forexCardsAndCurrency()
+    {
+
+    }
+    public void travelInsurance()
+    {
+
+    }
+    public void searchBtn()
+    {
+
+    }
+    public void listYourProperty()
+    {
+
+    }
+    public void myTrips()
+    {
+
+    }
+}
---
+++ src/main/java/PageClass/travelInsurancePage.java
@@ -0,0 +1,4 @@
+package PageClass;
+
+public class travelInsurancePage {
+}
